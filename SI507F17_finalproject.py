4a5a5c989f966701cb034bc795e4b0f3318b002c
ltsheridan <31678807+ltsheridan@users.noreply.github.com>
2017-12-13 21:42:50 -0500

    Adding in Database code

--- SI507F17_finalproject.py
+++ SI507F17_finalproject.py
@@ -8,7 +8,10 @@ import requests
 import unittest
 import psycopg2
 import psycopg2.extras
+import plotly
 from psycopg2 import sql
+# from flask import Flask, render_template
+# from flask_script import Manager
 from config import *
 
 #Links to access Spotify
@@ -90,7 +93,6 @@ related_artists_obj=[]
 for item in response_diction['artists']:
     artist_object=SpotifyArtist((item['name']), (item['popularity']),(item['images'][2]['url']))
     related_artists_obj.append(artist_object)
-
 # print(related_artists_obj)
 
 # artist_popularity=[]
@@ -102,9 +104,6 @@ for item in response_diction['artists']:
 # for item in response_diction['artists']:
 #     image_url.append(item['images'][2]['url'])
 # print(image_url)
-
-
-
 
 
 #Setting up the database
@@ -128,29 +127,102 @@ def get_connection_and_cursor():
 
     return db_connection, db_cursor
 
+db_connection, db_cursor = get_connection_and_cursor()
 
-# Code functions to create tables and database setup
+#Code functions to create tables and database setup
 
 def setup_database():
     conn, db_cursor = get_connection_and_cursor()
 
-    db_cursor.execute("DROP TABLE IF EXISTS Artists")
-    db_cursor.execute("DROP TABLE IF EXISTS Artists Popularity")
-    db_cursor.execute("DROP TABLE IF EXISTS Image URL")
 
+    db_cursor.execute("DROP TABLE IF EXISTS Popularity_and_Images")
+    db_cursor.execute("DROP TABLE IF EXISTS Artists")
 
     db_cursor.execute("CREATE TABLE Artists(ID SERIAL PRIMARY KEY, Artist VARCHAR (50) UNIQUE)")
-    db_cursor.execute("CREATE TABLE Artists Popularity(ID SERIAL PRIMARY KEY, Artist VARCHAR(50) UNIQUE, Artist_ID INTEGER REFERENCES Artist (ID), Artists Popularity INTEGER")
-    db_cursor.execute("CREATE TABLE Image URL(ID SERIAL PRIMARY KEY, Artist VARCHAR (50) UNIQUE, Image URL VARCHAR (255))")
+    db_cursor.execute("CREATE TABLE Popularity_and_Images(ID SERIAL PRIMARY KEY, Artist VARCHAR(50) UNIQUE, Popularity INTEGER, Image_URL TEXT)")
+    # db_cursor.execute("CREATE TABLE Images(ID SERIAL PRIMARY KEY, Artist VARCHAR (50) UNIQUE, Image_URL VARCHAR (255), Artist_ID INTEGER REFERENCES Artists (ID))")
+    #db_cursor.execute("CREATE TABLE Sites(ID SERIAL PRIMARY KEY, Name VARCHAR(128) UNIQUE, Type VARCHAR(128), State_ID INTEGER REFERENCES States (ID), Location VARCHAR(255), Description TEXT)")
     conn.commit()
 
-# Code to insert data into the database here.
 
-def insert_data(artist_name, popularity, url):
-    sql = "INSERT INTO Artists(Artist) VALUES(%s)"
-    db_cursor.execute(sql,(Artist))
-    db_connection.commit()
+# Code to insert data into the database here.
+def insert_artists_intodb(related_artists_obj):
+    for artist_obj in related_artists_obj:
+        artist_name = artist_obj.artist_name
+        popularity = artist_obj.popularity
+        image_url = artist_obj.image_url
+        sql = "INSERT INTO Artists(Artist) VALUES(%s) RETURNING id"
+        db_cursor.execute(sql,(artist_name,))
+        artist_id = db_cursor.fetchone()['id']
+        sql = "INSERT INTO Popularity_and_Images(Artist, Popularity, Image_URL) VALUES(%s,%s,%s)"
+        db_cursor.execute(sql,(artist_name, popularity,image_url))
+        db_connection.commit()
+
+
+# def insert_popandimage_intodb(popularity, image_url):
+#     sql = "INSERT INTO Popularity_and_Images(Artist, Popularity, Image_URL, artist_id) VALUES(%s,%s,%s,%s)"
+#     db_cursor.execute(sql,(popularity,image_url))
+#     db_connection.commit()
+#     return id
+
+# def insert_artist_intodb(artist_name):
+#     sql = "INSERT INTO Artists(Artist) VALUES(%s) RETURNING id"
+#     db_cursor.execute(sql,(artist_name,))
+#     db_connection.commit()
+#     return id
+#
+# def insert_pop_intodb(popularity):
+#     sql = "INSERT INTO Popularity(Artist, Popularity) VALUES(%s,%s)"
+#     db_cursor.execute(sql,(popularity,))
+#     artist_id = db_cursor.fetchone()['id']
 
 # Code to invoke functions
+
 get_connection_and_cursor()
 setup_database()
+insert_artists_intodb(related_artists_obj)
+
+# for artist_obj in related_artists_obj:
+#     insert_artist_intodb(artist_obj.artist_name)
+#     insert_pop_intodb(artist_obj.popularity)
+#
+# for artist_obj in related_artists_obj:
+#     insert_popandimage_intodb(artist_obj.popularity, artist_obj.image_url)
+#     # insert_pop_intodb(artist_obj.popularity)
+
+# Code for queries
+
+def execute(query, numer_of_results=1):
+    db_cursor.execute(query)
+    results = db_cursor.fetchall()
+
+
+
+#Visualizing the data
+#
+# app = Flask(__name__)
+#
+# manager = Manager(app)
+#
+# @app.route('/')
+# def hello_world():
+#     return '<h1>Hello World!</h1>'
+#
+# @app.route('/user/<yourname>')
+# def hello_name(yourname):
+#     return '<h1>Hello {}</h1>'.format(yourname)
+#
+# @app.route('/showvalues/<name>')
+# def basic_values_list(name):
+#     lst = ["hello","goodbye","tomorrow","many","words","jabberwocky"]
+#     if len(name) > 3:
+#         longname = name
+#         shortname = None
+#     else:
+#         longname = None
+#         shortname = name
+#     return render_template('values.html',word_list=lst,long_name=longname,short_name=shortname)
+#
+#
+# if __name__ == '__main__':
+#     manager.run() # Runs the flask server in a special way that makes it nice to debug
